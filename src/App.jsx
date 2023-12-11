a2717c0e1621c35d8da2bbcc54bd93ec5d264498
Samuel-Tobi <oluwatobi9990@gmail.com>
2023-12-11 13:31:22 +0100

    Created sidebar component and added tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Sibar from "./components/Sibar.jsx";
+
+function App() {
+  return (
+    <main className="h-screen my-8">
+      <Sibar />
+    </main>
+  );
+}
+
+export default App;

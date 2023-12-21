dae7d32df1cf930271a39a2ebe427bf2c35eeaa2
Samuel-Tobi <oluwatobi9990@gmail.com>
2023-12-21 15:24:57 -0600

    Created Input and NewProject component and styled them using tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import NewProject from "./components/NewProject.jsx";
 import Sibar from "./components/Sibar.jsx";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <Sibar />
+      <NewProject />
     </main>
   );
 }

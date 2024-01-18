ebe8a33f7110c11be6565655124959870c902270
Samuel-Tobi <oluwatobi9990@gmail.com>
2024-01-18 00:44:58 -0600

    clear tasks, and add new task to sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,14 @@ function App() {
       };
     });
   }
-  function handleDeleteTask() {}
+  function handleDeleteTask(taskId) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== taskId),
+      };
+    });
+  }
 
   function handleSelectProject(id) {
     setProjectsState((prevState) => {
@@ -112,6 +119,7 @@ function App() {
         onStartAddProject={handleStartAddProject}
         projects={projectsState.projects}
         onSelectProject={handleSelectProject}
+        selectedProjectId={projectsState.selectedProjectId}
       />
       {content}
     </main>
